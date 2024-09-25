ea8df6f1d82c29b77e6e93e6d935f11079034a51
Praween Chhetri <62017020+Pra-wnn@users.noreply.github.com>
2024-09-25 15:40:54 +0545

    Initial Commit

---
+++ db-dump/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS mydbc;
+USE mydbc;
+-- CREATE TABLE IF NOT EXISTS users (
+--        id INT AUTO_INCREMENT PRIMARY KEY,
+--        name VARCHAR(255),
+--        age INT
+-- );
+
